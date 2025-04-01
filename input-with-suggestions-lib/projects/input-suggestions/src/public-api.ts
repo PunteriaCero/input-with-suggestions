50485b1454684b57e7e6a88f68ae334ae33d894c
Mateo Codesido <mcodesido@baufest.com>
2025-04-01 09:47:46 -0300

    app

---
+++ input-with-suggestions-lib/projects/input-suggestions/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of input-suggestions
+ */
+
+export * from './lib/input-suggestions.service';
+export * from './lib/input-suggestions.component';
+export * from './lib/input-suggestions.module';
