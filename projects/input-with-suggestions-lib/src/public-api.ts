e53bcc77b8e620b03ec206cf05786552362ae46e
Mateo Codesido <mcodesido@baufest.com>
2025-04-07 17:22:01 -0300

    fix reorg files

--- projects/input-with-suggestions-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of input-with-suggestions
- */
-
-export * from './lib/input-with-suggestions.service';
-export * from './lib/input-with-suggestions.component';
-export * from './lib/input-with-suggestions.module';
