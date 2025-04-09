c1f6876084ec1bc21b743552eee875ab35df7210
Mateo Codesido <mcodesido@baufest.com>
2025-04-09 12:18:59 -0300

    FIX public api

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,3 @@
-export * from './input-with-suggestions.component';
-export * from './input-with-suggestions.service';
-export * from './input-with-suggestions.module'; 
+export * from './lib/input-with-suggestions.component';
+export * from './lib/input-with-suggestions.service';
+export * from './lib/input-with-suggestions.module';
