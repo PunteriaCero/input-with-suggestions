e0d24c6561ff79b819d299c2c64a2091dd1e00ee
Mateo Codesido <mcodesido@baufest.com>
2025-04-09 16:16:25 -0300

    fix config for version error

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,7 @@
+/*
+ * Public API Surface of input-with-suggestions-lib
+ */
+
 export * from './lib/input-with-suggestions.component';
 export * from './lib/input-with-suggestions.service';
 export * from './lib/input-with-suggestions.module';
